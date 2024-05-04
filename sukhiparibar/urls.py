c6da118c30d27a57e68d6676625fbfb95c9ad3bf
Washim Ahmed <washim.ahmed@gmail.com>
2024-05-04 17:16:03 +0000

    bug fix

--- sukhiparibar/urls.py
+++ sukhiparibar/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('', admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 admin.site.site_header = 'Sukhiparibar Console'
